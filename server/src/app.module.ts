cf3d3fcc5d062079ebf2ea472a5b8ad1e6149f67
GabrielBdeC <gabrielbdec@gmail.com>
2022-09-30 18:07:32 -0300

    feat: environment variable for database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,18 +3,15 @@ import { AuthModule } from './modules/auth/auth.module';
 import { DoorLockerUserModule } from './modules/door-locker-user/door-locker-user.module';
 import { HealthCheckModule } from './modules/health-check/health-check.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigService } from './core/services/typeorm-config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'db_arduino_locker_user',
-      password: 'ZGoOJkYuON0KkAa3',
-      database: 'db_arduino_locker',
-      entities: [],
-      synchronize: false,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      inject: [TypeOrmConfigService],
     }),
     AuthModule,
     DoorLockerUserModule,
---
+++ server/src/core/services/typeorm-config.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: this.configService.get<any>('TYPEORM_TYPE'),
+      host: this.configService.get<string>('TYPEORM_HOST'),
+      port: this.configService.get<number>('TYPEORM_PORT'),
+      username: this.configService.get<string>('TYPEORM_USERNAME'),
+      password: this.configService.get<string>('TYPEORM_PASSWORD'),
+      database: this.configService.get<string>('TYPEORM_DATABASE'),
+      entities: [
+        'dist/app/module/*/entity/*.entity.js',
+        'dist/app/shared/*/entity/*.entity.js',
+      ],
+      synchronize: false,
+    };
+  }
+}
