add1fde0e5648940d835fe9aee339f3a69add0a9
GabrielBdeC <gabrielbdec@gmail.com>
2022-09-30 17:55:48 -0300

    feat: database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { DoorLockerUserModule } from './modules/door-locker-user/door-locker-user.module';
 import { HealthCheckModule } from './modules/health-check/health-check.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, DoorLockerUserModule, HealthCheckModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'db_arduino_locker_user',
+      password: 'ZGoOJkYuON0KkAa3',
+      database: 'db_arduino_locker',
+      entities: [],
+      synchronize: false,
+    }),
+    AuthModule,
+    DoorLockerUserModule,
+    HealthCheckModule,
+  ],
 })
 export class AppModule {}
