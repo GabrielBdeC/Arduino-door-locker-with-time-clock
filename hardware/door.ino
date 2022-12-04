f68e991980775c901538de4ccf6b21567b1d6749
Tdlreis <tdlreis@gmail.com>
2022-12-04 01:31:42 -0300

    HArdware completamente implementado

--- hardware/door.ino
+++ hardware/door.ino
@@ -8,6 +8,9 @@
 //Include RFID reader
 #include <MFRC522.h>
 
+//Server url
+#define URL "http://192.168.3.8:3000/api/door_locker/"
+
 //Config RfID ports
 #define SS_PIN 32
 #define RST_PIN 33
@@ -16,21 +19,21 @@
 #define button 2
 #define locker 4
 
-//Server url
-#define URL "http://192.168.3.8:3000/api/door_locker/"
- 
 //Classes
 HTTPClient http;
 MFRC522 rfid(SS_PIN, RST_PIN); 
 MFRC522::MIFARE_Key key;
 LiquidCrystal_I2C lcd(0x27, 20, 04);
-hw_timer_t* timer = NULL;
+hw_timer_t* refreshTimer = NULL;
+hw_timer_t* reconnectTimer = NULL;
+int reconnectTimerTime = 1000000;
 
 
 //Variables
 String token;
-size_t safeRepetitionToken = 0;
-size_t safeRepetitionUpdateToken = 0;
+size_t safeRepetition = 0;
+
+
 
 //Functions
 //Wifi Setup
@@ -76,35 +79,72 @@ bool getProtectedHealth(){
 bool errorHandler(int errorCode, int errorPlace){
 	//1-getToken
 	//2-isOnBd
-	if(errorCode == 401){
-        getToken();
+	if(errorCode == 401 && safeRepetition < 5){
+        safeRepetition++;
+		getToken();
+		return true;
     }
 	else if(errorCode == 500){	//Server Internal Error
 		lcd.clear();
-        lcd.setCursor(7,0);
-        lcd.print("LabTeC");
+        lcd.setCursor(0,0);
+        lcd.print("Erro: 500");
         lcd.setCursor(0,1);
-        lcd.print("Falha interna");
+        lcd.print("Erro Interno");
         lcd.setCursor(0,2);
         lcd.print("no Servidor");
-        lcd.setCursor(0,3);
-        lcd.print("Servidor");
 	}
-	else if(errorCode == 400){
-		//Bad request -> Algo errado porra
+	else if(errorCode == 400){	//Bad request -> Algo errado porra
+		lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Erro: 400");
+        lcd.setCursor(0,1);
+        lcd.print("Requesição Ruim");
+        lcd.setCursor(0,2);
+		if(errorPlace = 1){
+        	lcd.print("Local: Login");
+		}
+		else if(errorPlace == 2){
+			lcd.print("Local: Autenticação");
+		}
 	}
 	else if(errorCode < 0){	//Server not reached
 		
 		lcd.clear();
-        lcd.setCursor(7,0);
-        lcd.print("LabTeC");
+        lcd.setCursor(0,0);
+        lcd.print("Erro: -1");
         lcd.setCursor(0,1);
-        lcd.print("Falha ao conectar");
+        lcd.print("Não foi possivel");
         lcd.setCursor(0,2);
-        lcd.print("com o");
-        lcd.setCursor(0,3);
-        lcd.print("Servidor");
+        lcd.print("acessar o servidor");
 	}
+
+	timerAlarmWrite(reconnectTimer, reconnectTimerTime, false);
+	timerStart(reconnectTimer);
+	lcd.setCursor(0,3);
+	int timeLeft;
+	while(timerStarted(reconnectTimer)){
+        timeLeft = reconnectTimerTime-timerRead(reconnectTimer);
+        if (timeLeft > 1000000)
+        {
+           lcd.print("Reconetando em: " + String(timeLeft/1000000) + "s");
+        }
+        else{
+           lcd.print("Reconetando em: " + String(timeLeft/1000) + "ms");
+        }
+	}
+
+    if (reconnectTimerTime < 30000000)
+	{
+		reconnectTimerTime += 1000000;		
+	}
+	getToken();
+	return false;
+}
+
+//Reconect
+void IRAM_ATTR reconect(){
+	timerStop(reconnectTimer);
+	timerRestart(reconnectTimer);
 }
 
 //Get Token
@@ -119,7 +159,7 @@ void getToken(){
         int index2 = payload.indexOf("\"");
         payload.remove(index2, payload.length());
         token = payload;
-        safeRepetitionToken = 0;
+        safeRepetition = 0;
     }
     else {
 		errorHandler(httpPost, 1);
@@ -128,20 +168,19 @@ void getToken(){
 }
 
 //Checks if RFID is valid on Data Base
-void isOnDd(String rfid){
-    http.begin(String(URL) + "/v1/locker");
+void isOnDb(String rfidTag){
+    http.begin(String(URL) + "v1/locker");
     String bearer = "Bearer ";
     bearer += token;
     http.addHeader("Authorization", "Bearer " + token);
     http.addHeader("Content-Type", "application/json");
-    int httpPost = http.POST("{\"rfid\": \"" + rfid + "\"}");
+    int httpPost = http.POST("{\"rfid\": \"" + rfidTag + "\"}");
 
     if(httpPost  == 200){
-        safeRepetitionUpdateToken = 0;
+        safeRepetition = 0;
         String payload = http.getString();
-        http.end();
         
-        if (payload == "OK")
+        if (payload == "STORED")
         {
             lcd.clear();
             lcd.setCursor(7,0);
@@ -150,7 +189,7 @@ void isOnDd(String rfid){
             lcd.print("Cartão enviado");
             lcd.setCursor(0,3);
             lcd.print("para cadastro");
-            timerRestart(timer);
+            timerRestart(refreshTimer);
         }
         else{
             lcd.clear();
@@ -177,7 +216,6 @@ void isOnDd(String rfid){
                 lcd.setCursor(0,3);
                 lcd.print(payload);
             } 
-            timerRestart(timer);
             digitalWrite(locker, HIGH);
             delay(100);
             digitalWrite(locker, LOW);
@@ -197,14 +235,16 @@ void isOnDd(String rfid){
     }
     else{
 		if(errorHandler(httpPost, 2)){
-			isOnDd(rfid);
+			isOnDb(rfidTag);
 		}
-		http.end();
 	}
+	http.end();
+	timerRestart(refreshTimer);
+    timerStart(refreshTimer);
 }
 
 //LCD reset
-void lcdReset(){
+void IRAM_ATTR lcdReset(){
     lcd.clear();
     lcd.setCursor(7,0);
     lcd.print("LabTeC");
@@ -212,29 +252,41 @@ void lcdReset(){
     lcd.print("Aproxime seu cartão");
     lcd.setCursor(0,3);
     lcd.print("ao lado do visor");
-    timerStop(timer);
+    timerStop(refreshTimer);
 }
 
 //Start of the program
 //Setup
 void setup(){
+    refreshTimer = timerBegin(0, 80, true);                		// timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
+    timerAttachInterrupt(refreshTimer, &lcdReset, true);   		// edge (not level) triggered 
+    timerAlarmWrite(refreshTimer, 5000000, true);          	// 1000000 * 1 us = 1 s, autoreload true
+    timerAlarmEnable(refreshTimer);                        		// enable
+    timerStop(refreshTimer);
+
+    reconnectTimer = timerBegin(1, 80, true);                	// timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
+    timerAttachInterrupt(reconnectTimer, &reconect, true);   	// edge (not level) triggered 
+    timerAlarmWrite(reconnectTimer, reconnectTimerTime, false);  // 1000000 * 1 us = 1 s, autoreload true
+    timerAlarmEnable(reconnectTimer);                        	// enable
+    timerStop(reconnectTimer);
+
     // Serial.begin(115200);    
     wifiSetup();
+
     getToken();
+
     pinMode(button, INPUT);
     pinMode(locker, OUTPUT);
+
     rfid.PCD_Init();
+
     lcd.init();
     lcd.backlight();
     lcdReset();
-    timer = timerBegin(0, 80, true);                // timer 0, MWDT clock period = 12.5 ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 -> 1000 ns = 1 us, countUp
-    timerAttachInterrupt(timer, &lcdReset, true);   // edge (not level) triggered 
-    timerAlarmWrite(timer, 5000000, true);          // 1000000 * 1 us = 1 s, autoreload true
-    timerAlarmEnable(timer);                        // enable
-    timerStop(timer);
 }
 
 void loop(){
+	reconnectTimerTime = 0;
     if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()){
         String cardUID;
         for (size_t i = 0; i < rfid.uid.size; i++)
@@ -242,6 +294,6 @@ void loop(){
             cardUID.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
             cardUID.concat(String(rfid.uid.uidByte[i], HEX));
         }
-        isOnDd(cardUID);
+        isOnDb(cardUID);
     }
 }
